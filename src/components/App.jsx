fb53f43c0623b80907bfc0a15faa649eb07150b5
lostgirljourney <falgunisarkar526@gmail.com>
2021-08-22 22:25:29 +0530

    Assets, Codes and README Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import RandomQuote from "./RandomQuote/RandomQuote";
 
 export default function App() {
-  return (
-    <div className="App">
-      <RandomQuote />
-    </div>
-  );
+	return (
+		<div className="App">
+			<RandomQuote />
+		</div>
+	);
 }
