605f992f8d9442191a3c994c1245c409f17928f7
Falguni Sarkar <falgunisarkar526@gmail.com>
2021-09-08 12:43:34 +0530

    Assets, Codes and README Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer() {
     return (
         <div className="footer-section">
-            created by <a href="https://devchallenges.io/portfolio/lostgirljourney" target="_blank" className="username" rel="noopener noreferrer">@lostgirljourney</a>
+            created by <a href="https://devchallenges.io/portfolio/lostgirljourney" target="_blank" className="username" rel="noopener noreferrer">@lostgirljourney </a>
             -
             devChallenges.io
         </div>
